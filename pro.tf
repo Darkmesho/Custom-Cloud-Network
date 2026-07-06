f7fc04c0f1a9015747a49a5348feed2b086e0682
Ahmed-S-06 <ahmedsamikeshta@gmail.com>
2026-07-06 17:07:48 +0000

    Add mi initial infrastructure files

--- pro.tf
+++ pro.tf
@@ -1,11 +1,34 @@
 # at here we wrote the provider and the region of servers
+
 provider"aws"{
+
 region = "us-east-1"
+
 }
 
-# at here we wrote the first resource  and the type 
-resource"aws_vpc" "the great wall of vpc"{
+# at here we wrote the first resource  and the type and name the and area  of the net
+ 
+resource"aws_vpc" "the_great_wall_of_vpc"{
 
 cidr="10.0.0.0/16"
 
+}
+
+#we make a new resource with name my.... 
+
+resource "aws_subnet" "my subnet"{
+
+vpc_id=aws_vpc.the_great_wall_of_vpc.id
+
+#we tell him to get the id of VPC  from above
+
+cidr_block = "10.0.1.0/24"
+
+map_public_ip_on_lanch=true
+}
+# this line make  the server take  any ip  to make it seen on the internet
+
+resource "aws_internet_gateway" "my_ige"{
+# make a gate
+vpc_id = aws_vpc.the_great_wall_of_vpc.id
 }
