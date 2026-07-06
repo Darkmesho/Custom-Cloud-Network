07c44d5b75824f114dcd85fb589420459e21d4e4
Ahmed-S-06 <ahmedsamikeshta@gmail.com>
2026-07-06 22:53:55 +0000

    Add mi initial infrastructure files

--- pro.tf
+++ pro.tf
@@ -1,3 +1,11 @@
+# the first of all we want to build a compound  project so the first we will take a peace of land "provider" second we make the walls "vpc"
+#
+
+
+
+
+
+
 # at here we wrote the provider and the region of servers
 
 provider"aws"{
@@ -10,13 +18,13 @@ region = "us-east-1"
  
 resource"aws_vpc" "the_great_wall_of_vpc"{
 
-cidr="10.0.0.0/16"
+cidr_block="10.0.0.0/16"
 
 }
 
 #we make a new resource with name my.... 
 
-resource "aws_subnet" "my subnet"{
+resource "aws_subnet" "my_subnet"{
 
 vpc_id=aws_vpc.the_great_wall_of_vpc.id
 
@@ -24,11 +32,11 @@ vpc_id=aws_vpc.the_great_wall_of_vpc.id
 
 cidr_block = "10.0.1.0/24"
 
-map_public_ip_on_lanch=true
+map_public_ip_on_launch=true
 }
 # this line make  the server take  any ip  to make it seen on the internet
 
-resource "aws_internet_gateway" "my_ige"{
+resource "aws_internet_gateway" "my_igw"{
 # make a gate
 vpc_id = aws_vpc.the_great_wall_of_vpc.id
 }
@@ -38,8 +46,13 @@ resource"aws_route_table" "my_routetable"{
 vpc_id =aws_vpc.the_great_wall_of_vpc.id
 
 route{
+# it is an inner block  that we put the base on it 
 cidr_block ="0.0.0.0/0"
+# any id from the internet
 gateway_id =aws_internet_gateway.my_igw.id
+# tell the 
 }
 }
-
+resource " aws_route_table_association" "my_association"{
+subnet_id = aws_subnet.my_subnet.id
+route_table_id =aws_route_table.my_routetable.id
