7e22f00591fe7432e222b5cbd3fe0b63055cb379
Darkmesho <madazahran3@gmail.com>
2026-07-07 03:27:25 +0300

    updated the project and refineded it

--- pro.tf
+++ pro.tf
@@ -1,59 +1,50 @@
-# the first of all we want to build a compound  project so the first we will take a peace of land "provider" second we make the walls "vpc"
-#
-
-
-
-
-
-
-# at here we wrote the provider and the region of servers
-
-provider"aws"{
-
-region = "us-east-1"
-
+# Provider and Region Configuration
+provider "aws" {
+  region = "us-east-1"
 }
 
-# at here we wrote the first resource  and the type and name the and area  of the net
- 
-resource"aws_vpc" "the_great_wall_of_vpc"{
+# Defining Private Ip range (VPC)
+ resource"aws_vpc" "the_great_wall_of_vpc" {
+  cidr_block = "10.0.0.0/16"
 
-cidr_block="10.0.0.0/16"
-
+# Tag for easier identification through the AWS web 
+  tags = {
+    name = "the_great_wall_of_vpc"
+  }
 }
 
-#we make a new resource with name my.... 
-
-resource "aws_subnet" "my_subnet"{
-
-vpc_id=aws_vpc.the_great_wall_of_vpc.id
-
-#we tell him to get the id of VPC  from above
-
-cidr_block = "10.0.1.0/24"
-
-map_public_ip_on_launch=true
-}
-# this line make  the server take  any ip  to make it seen on the internet
-
-resource "aws_internet_gateway" "my_igw"{
-# make a gate
-vpc_id = aws_vpc.the_great_wall_of_vpc.id
+# Creating subnet for servers 
+resource "aws_subnet" "server_subnet" {
+  vpc_id = aws_vpc.the_great_wall_of_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 }
 
-resource"aws_route_table" "my_routetable"{
-
-vpc_id =aws_vpc.the_great_wall_of_vpc.id
-
-route{
-# it is an inner block  that we put the base on it 
-cidr_block ="0.0.0.0/0"
-# any id from the internet
-gateway_id =aws_internet_gateway.my_igw.id
-# tell the 
+# Gateway creation
+resource "aws_internet_gateway" "Ark_igw" {
+  vpc_id = aws_vpc.the_great_wall_of_vpc.id
+  
+  tags = {
+    name = "sandbox-igw"
+  }
 }
+
+# Connecting the gateway through route table
+resource "aws_route_table" "layout_route_table" {
+  vpc_id = aws_vpc.the_great_wall_of_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Ark_igw.id
+  }
+
+  tags = {
+    name = "layout_route_table"
+  }
 }
-resource " aws_route_table_association" "my_association"{
-subnet_id = aws_subnet.my_subnet.id
-route_table_id =aws_route_table.my_routetable.id
+
+# Associating the route table with our subnet
+resource "aws_route_table_association" "Keshta_association" {
+  subnet_id = aws_subnet.server_subnet.id
+  route_table_id = aws_route_table.layout_route_table.id
 }
